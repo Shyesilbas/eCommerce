c96460c2513d8b33fab7c70984a676eccfadf4f8
Serhathy <shyesilbas@gmail.com>
2024-12-29 18:49:40 +0300

    Refactored User Details Service

--- src/main/java/com/serhat/security/service/UserDetailsServiceImpl.java
+++ src/main/java/com/serhat/security/service/UserDetailsServiceImpl.java
@@ -16,9 +16,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username)
-                .orElseThrow(()-> new UsernameNotFoundException("Username not found"));
+        return userRepository.findByUsername(username)
+                .orElseThrow(()-> new UsernameNotFoundException("User not found"));
 
-        return user;
     }
 }
