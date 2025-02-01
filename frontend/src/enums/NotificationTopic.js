84542b6461e302cb2ce21fdf84110a270ededd88
Shyesilbas <161944828+Shyesilbas@users.noreply.github.com>
2025-02-01 23:06:20 +0300

    Delete frontend directory

--- frontend/src/enums/NotificationTopic.js
+++
@@ -1,9 +0,0 @@
-export const NotificationTopic = {
-    ADDRESS_ADDED: "New Address Added",
-    ADDRESS_DELETED: "Address Deleted",
-    ADDRESS_UPDATED: "Address Update",
-    EMAIL_UPDATE: "E-Mail updated",
-    PASSWORD_UPDATE: "Password Updated",
-    PHONE_UPDATE: "Phone number update",
-    FAILED_LOGIN: "Login attempt failed!",
-};
